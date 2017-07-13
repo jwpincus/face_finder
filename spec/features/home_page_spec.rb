40f330d26ad0ece4aadd7238a8bac40571d2dce7
Jack Pincus <jwpincus@gmail.com>
2017-07-13 15:05:16 -0600

    first commit

---
+++ spec/features/home_page_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+feature 'A user visiting the home page' do
+  scenario 'can click on log in to be taken to log in screen' do
+    visit '/'
+    expect(page).to have_content('Log In')
+  end
+end
