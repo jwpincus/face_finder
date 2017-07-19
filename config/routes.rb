69960eef8e517d59a0c908fe5b83a8f86236c945
Jack Pincus <jwpincus@gmail.com>
2017-07-19 14:06:25 -0600

    add basic bcrypt login

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,13 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'welcome#index'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -3,5 +3,6 @@ feature 'A user visiting the home page' do
   scenario 'can click on log in to be taken to log in screen' do
     visit '/'
     expect(page).to have_content('Log In')
+    expect(page).to have_content('Register')
   end
 end
