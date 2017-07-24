3ac9518e6160959e53bbc6ab0b3ba659a2e58a60
Jack Pincus <jwpincus@gmail.com>
2017-07-24 13:54:48 -0600

    uncomments tests

--- spec/api/authentication_api_spec.rb
+++ spec/api/authentication_api_spec.rb
@@ -11,27 +11,27 @@ describe 'an api can', type: :api do
     @app.authorized_users << @user
   end
   context 'authenticate a user via api' do
-    # scenario 'GET /api/v1/authenticate with good image' do
-    #   response = post '/api/v1/authenticate', {
-    #     "app_id" => "#{@app.id}",
-    #     "image" => @test_image.image
-    #   }
-    #   body = JSON.parse(response.body)
-    #   expect(response.status).to eq(200)
-    #   expect(body['authenticated']).to eq(true)
-    #   expect(body['user']['name']).to eq(@user.first_name)
-    # end
-    #
-    # scenario 'GET /api/v1/authenticate with no faces rejects' do
-    #   response = post '/api/v1/authenticate', {
-    #     "app_id" => "#{@app.id}",
-    #     "image" => @test_image.bad_image
-    #   }
-    #   body = JSON.parse(response.body)
-    #   expect(response.status).to eq(200)
-    #   expect(body['authenticated']).to eq(false)
-    #   expect(body['error']).to eq('Unable to find a face that matched an authorized user')
-    # end
+    scenario 'GET /api/v1/authenticate with good image' do
+      response = post '/api/v1/authenticate', {
+        "app_id" => "#{@app.id}",
+        "image" => @test_image.image
+      }
+      body = JSON.parse(response.body)
+      expect(response.status).to eq(200)
+      expect(body['authenticated']).to eq(true)
+      expect(body['user']['name']).to eq(@user.first_name)
+    end
+
+    scenario 'GET /api/v1/authenticate with no faces rejects' do
+      response = post '/api/v1/authenticate', {
+        "app_id" => "#{@app.id}",
+        "image" => @test_image.bad_image
+      }
+      body = JSON.parse(response.body)
+      expect(response.status).to eq(200)
+      expect(body['authenticated']).to eq(false)
+      expect(body['error']).to eq('Unable to find a face that matched an authorized user')
+    end
 
     scenario 'GET /api/v1/authenticate rejects Donald Trump (or any stranger)' do
       response = post '/api/v1/authenticate', {
