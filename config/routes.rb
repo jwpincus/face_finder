c436af5a49e4262c7ad92b078fdcc974f41ec010
Jack Pincus <jwpincus@gmail.com>
2017-07-25 15:51:53 -0600

    add dashboard tab and add path to create additional enrollments

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'welcome#index'
+  root 'dashboard#index'
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
